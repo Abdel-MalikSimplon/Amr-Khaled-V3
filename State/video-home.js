ce7a43598df58fe94dabf7b18b6c8ce3ba35bbe8
Abdel-Malik <Ushiden13@gmail.com>
2016-09-28 22:30:11 +0200

    Ajout du module video avec le lecteur qui estdonc modulaire grâce à data-video dans le html et dataset.video en js. Ajout également du Twitter Card. Modification des boutons des réseaux sociaux ainsi que des boutons français/anglais.

--- State/video-home.js
+++ State/video-home.js
@@ -3,6 +3,6 @@
     var btn = document.querySelector('.btn-play')
 
     btn.addEventListener('click', function() {
-        blocMea.innerHTML = '<iframe width="960" height="515" src="https://www.youtube.com/embed/Zu8c6RdRTaI?autoplay=1" frameborder="0" allowfullscreen></iframe>'
+    	blocMea.innerHTML = '<iframe width="960" height="515" src="https://www.youtube.com/embed/' + this.dataset.video + '?autoplay=1" frameborder="0" allowfullscreen></iframe>'
     })
 })()
