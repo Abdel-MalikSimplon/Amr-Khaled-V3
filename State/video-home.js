05d282a4266f1108505168913bcf32fc91c79931
Abdel-Malik <Ushiden13@gmail.com>
2016-12-29 20:49:03 +0100

    Amélioration du site et du code JavaScript

--- State/video-home.js
+++ State/video-home.js
@@ -1,8 +1,10 @@
 (function() {
-    var blocMea = document.querySelector('.bloc-video-mea')
-    var btn = document.querySelector('.btn-play')
+
+    var blocMea = document.querySelector('.bloc-video-mea');
+    var btn = document.querySelector('.btn-play');
 
     btn.addEventListener('click', function() {
     	blocMea.innerHTML = '<iframe width="960" height="515" src="https://www.youtube.com/embed/' + this.dataset.video + '?autoplay=1" frameborder="0" allowfullscreen></iframe>'
-    })
-})()
+    });
+
+})();
