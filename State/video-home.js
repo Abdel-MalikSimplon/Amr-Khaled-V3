e57f20971d156b682ed42e522a9a65162a066d1c
Abdel-Malik <Ushiden13@gmail.com>
2017-01-01 14:53:07 +0100

    Modification du fichier layout.css pour le curseur indiquant que l'épisode n'est pas encore disponible ainsi que le texte des vidéos et le fichier index.html

--- State/video-home.js
+++ State/video-home.js
@@ -8,10 +8,24 @@
     });
 
     const allEmissions = document.querySelectorAll('.all-emissions li > a');
-    console.log(allEmissions)
 
     for (var i = 0; i < allEmissions.length; i++) {
         allEmissions[i].addEventListener('click', function(e) {
+            e.preventDefault();
+        })
+    }
+
+    const allEmissionsHome = document.querySelectorAll('.all-emission-home a');
+    const btnVoirPlus = document.querySelectorAll('.btn-voir-plus');
+
+    for (var i = 0; i < allEmissionsHome.length; i++) {
+        allEmissionsHome[i].addEventListener('click', function(e){
+            e.preventDefault();
+        });
+    }
+
+    for (var i = 0; i < btnVoirPlus.length; i++) {
+        btnVoirPlus[i].addEventListener('click', function(e) {
             e.preventDefault();
         })
     }
